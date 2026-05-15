f119d5344c9fad20d5262899863c8dadd218737c
Contato Propertech <contato@propertech.com.br>
2026-05-15 16:26:45 -0300

    fazendo incerções em C#

--- hello_world.cs
+++ hello_world.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine ("Hello, World!");
-string aFriend = "Bill";
+﻿string aFriend = "Bill";
 Console.WriteLine (aFriend);
+aFriend = "João";
+Console.WriteLine (aFriend);
+Console.WriteLine ($"Hello {aFriend}");
+
+string firstFriend = "Maria";
+string secondFriend = "Bianca";
+Console.WriteLine ($"My friends are {firstFriend} and {secondFriend}");
--- obj/Debug/net10.0/treinando-em-c.AssemblyInfo.cs
+++ obj/Debug/net10.0/treinando-em-c.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("treinando-em-c")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c9c471ff189ef0745f5e164466b6fe84215645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21258a7615c70a03b63005ff5db07e9f00f96a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("treinando-em-c")]
 [assembly: System.Reflection.AssemblyTitleAttribute("treinando-em-c")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
